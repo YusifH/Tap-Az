d1369bb1b9aabaff30d94ff3320fca7ac5ca0ad8
Yusif Hajiyev <yusifhajiyev@yandex.com>
2022-03-22 19:29:31 +0400

    Splash Screen

---
+++ lib/contsants/color.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class MyTheme {
+  static const splash = Color(0xFFff4f08);
+  static const statusBar = Color(0xFF2E3147);
+  static const appBarColor = Color(0xFF222539);
+  static const greenColor = Color(0xFF2EC492);
+  static const orangeColor = Color(0xFFEB8D2F);
+  static const greyColor = Color(0xFFF4F4F4);
+  static const blueBorder = Color(0xFF3164CE);
+  static const redBorder = Color(0xFFF14336);
+  static const redLight = Color(0xFFFFF1F0);
+  static const blueLight = Color(0xFFF5F9FF);
+
+  static const redTextColor = Color(0xFFD05045);
+  static const greenTextColor = Color(0xFF8CC153);
+
+}
